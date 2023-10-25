1b9beceb2a6de7fac4e375d2034a0f120f0c6000
pepegajk <alekseje2086@gmail.com>
2023-10-25 21:59:14 +0300

    dsa

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Order
+{
+    private string shape;
+    private string size;
+    private string flavor;
+    private int quantity;
+    private string glazing;
+    private string decoration;
+
+    public void StartOrder()
+    {
+        Console.WriteLine("Добро пожаловать в CandyDanya! Что бы вы хотели заказать?");
+
+        shape = ShowMenuAndGetSelection("Форма", new List<string> { "Круглый ", "Прямоугольный", "Ваша форма(обсудить по горячей линии 89778125252)" });
+        size = ShowMenuAndGetSelection("Размер", new List<string> { "Маленький", "Средний", "Большой", "Огромный" });
+        flavor = ShowMenuAndGetSelection("Вкус", new List<string> { " Шоколадный", "Ванильный", " Фруктовый", "Ореховый", "Веганский" });
+
+        Console.Write("Введите количество: ");
+        quantity = int.Parse(Console.ReadLine());
+
+        glazing = ShowMenuAndGetSelection("Глазурь", new List<string> { "Шоколадная", "Сливочная", "Фруктовая" });
+        decoration = ShowMenuAndGetSelection("Декор", new List<string> { "Цветы", "Фигурки", " Шоколадные детали", "Несъедобные детали(обсудить по горячей линии 89778125252)" });
+
+        Console.WriteLine("Спасибо за заказ! Итоговая цена: " + CalculatePrice());
+
+        SaveOrderToFile();
+
+        Console.WriteLine(" Хотите оформить еще один заказ? Это 2?");
+        string answer = Console.ReadLine().ToLower();
+
+        if (answer == "y")
+        {
+            StartOrder();
+        }
+        else
+        {
+            Console.WriteLine("Спасибо за покупку! ");
+        }
+    }
+
+    private string ShowMenuAndGetSelection(string menuItem, List<string> submenuItems)
+    {
+        Console.WriteLine("Выберите " + menuItem);
+
+        int selectedIdx = 0;
+        int maxIdx = submenuItems.Count - 1;
+
+        while (true)
+        {
+            for (int i = 0; i <= maxIdx; i++)
+            {
+                if (i == selectedIdx)
+                {
+                    Console.WriteLine("> " + submenuItems[i]);
+                }
+                else
+                {
+                    Console.WriteLine("  " + submenuItems[i]);
+                }
+            }
+
+            ConsoleKeyInfo key = Console.ReadKey();
+
+            if (key.Key == ConsoleKey.UpArrow)
+            {
+                if (selectedIdx > 0)
+                {
+                    selectedIdx--;
+                }
+            }
+            else if (key.Key == ConsoleKey.DownArrow)
+            {
+                if (selectedIdx < maxIdx)
+                {
+                    selectedIdx++;
+                }
+            }
+            else if (key.Key == ConsoleKey.Enter)
+            {
+                Console.WriteLine();
+                return submenuItems[selectedIdx];
+            }
+        }
+    }
+
+    private decimal CalculatePrice()
+    {
+        decimal price = 0;
+
+        if (shape == "Круглый")
+        {
+            price += 300;
+        }
+        else if (shape == "Прямоугольный")
+        {
+            price += 250;
+        }
+        else if (shape == "Ваша форма")
+        {
+            price += 600;
+        }
+
+        if (size == "Маленький")
+        {
+            price += 700;
+        }
+        else if (size == "Средний")
+        {
+            price += 1500;
+        }
+        else if (size == "Большой")
+        {
+            price += 2000;
+        }
+        else if (size == "Огромный")
+        {
+            price += 4000;
+        }
+
+        price += 10.5m * quantity;
+
+        return price;
+    }
+
+    private void SaveOrderToFile()
+    {
+        string filename = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".txt";
+        using (StreamWriter sw = new StreamWriter(filename))
+        {
+            sw.WriteLine("Форма: " + shape);
+            sw.WriteLine("Размер: " + size);
+            sw.WriteLine("Вкус: " + flavor);
+            sw.WriteLine("Количество: " + quantity);
+            sw.WriteLine("Глазурь: " + glazing);
+            sw.WriteLine("Декор: " + decoration);
+        }
+
+        Console.WriteLine("Заказ сохранен в файл " + filename);
+    }
+}
---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Order
+{
+    private string form;
+    private string size;
+    private string flavor;
+    private int quantity;
+    private string icing;
+    private string decoration;
+
+    public void StartOrder()
+    {
+        Console.WriteLine("=== Cake Order Menu ===");
+        Console.WriteLine("1. Form");
+        Console.WriteLine("2. Size");
+        Console.WriteLine("3. Flavor");
+        Console.WriteLine("4. Quantity");
+        Console.WriteLine("5. Icing");
+        Console.WriteLine("6. Decoration");
+        Console.WriteLine("Esc. Exit");
+
+        bool exit = false;
+        while (!exit)
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.Enter:
+                    ExecuteSelectedMenuItem();
+                    break;
+                case ConsoleKey.Escape:
+                    exit = true;
+                    break;
+            }
+        }
+    }
+
+    private void ExecuteSelectedMenuItem()
+    {
+        Console.Clear();
+        Console.WriteLine("=== Selected Option ===");
+        Console.WriteLine("1. Form");
+        Console.WriteLine("2. Size");
+        Console.WriteLine("3. Flavor");
+        Console.WriteLine("4. Quantity");
+        Console.WriteLine("5. Icing");
+        Console.WriteLine("6. Decoration");
+        Console.WriteLine("Esc. Exit");
+
+        ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+        switch (keyInfo.Key)
+        {
+            case ConsoleKey.Escape:
+                Console.Clear();
+                StartOrder();
+                break;
+            case ConsoleKey.D1:
+                SelectForm();
+                break;
+            case ConsoleKey.D2:
+                SelectSize();
+                break;
+            case ConsoleKey.D3:
+                SelectFlavor();
+                break;
+            case ConsoleKey.D4:
+                SelectQuantity();
+                break;
+            case ConsoleKey.D5:
+                SelectIcing();
+                break;
+            case ConsoleKey.D6:
+                SelectDecoration();
+                break;
+            default:
+                ExecuteSelectedMenuItem();
+                break;
+        }
+    }
+
+    private void SelectForm()
+    {
+        Console.WriteLine("=== Select Form ===");
+        // Вывод доступных форм тортов
+
+        // Здесь можно реализовать логику выбора формы торта
+
+        ExecuteSelectedMenuItem();
+    }
+
+    private void SelectSize()
+    {
+        Console.WriteLine("=== Select Size ===");
+        // Вывод доступных размеров тортов
+
+        // Здесь можно реализовать логику выбора размера торта
+
+        ExecuteSelectedMenuItem();
+    }
+
+    private void SelectFlavor()
+    {
+        Console.WriteLine("=== Select Flavor ===");
+        // Вывод доступных вкусов тортов
+
+        // Здесь можно реализовать логику выбора вкуса торта
+
+        ExecuteSelectedMenuItem();
+    }
+
+    private void SelectQuantity()
+    {
+        Console.WriteLine("=== Select Quantity ===");
+        // Здесь можно реализовать логику выбора количества тортов
+
+        ExecuteSelectedMenuItem();
+    }
+
+    private void SelectIcing()
+    {
+        Console.WriteLine("=== Select Icing ===");
+        // Вывод доступных видов глазурей
+
+        // Здесь можно реализовать логику выбора глазури
+
+        ExecuteSelectedMenuItem();
+    }
+
+    private void SelectDecoration()
+    {
+        Console.WriteLine("=== Select Decoration ===");
+        // Вывод доступных вариантов декорации
+
+        // Здесь можно реализовать логику выбора декорации
+
+        ExecuteSelectedMenuItem();
+    }
+
+    private void SaveOrderToFile()
+    {
+        string filePath = "История заказов.txt";
+        string orderDetails = $"Form: {form}, Size: {size}, Flavor: {flavor}, Quantity: {quantity}, Icing: {icing}, Decoration: {decoration}";
+        string orderSummary = $"Order: {orderDetails}, Total Price: {CalculateTotalPrice()}";
+
+        using (StreamWriter writer = new StreamWriter(filePath, true))
+        {
+            writer.WriteLine(orderSummary);
+        }
+    }
+
+    private decimal CalculateTotalPrice()
+    {
+        // Здесь можно реализовать расчет общей стоимости заказа на основе выбранных параметров
+
+        return 0; // Вернуть общую стоимость заказа
+    }
+}
+
+class MenuItem
+{
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+
+    public MenuItem(string description, decimal price)
+    {
+        Description = description;
+        Price = price;
+    }
+}
+
+class Menu
+{
+    private List<MenuItem> subMenuItems;
+
+    public Menu(List<MenuItem> items)
+    {
+        subMenuItems = items;
+    }
+
+    public void ExecuteSubMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("=== Submenu ===");
+
+        for (int i = 0; i < subMenuItems.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {subMenuItems[i].Description} - {subMenuItems[i].Price}$");
+        }
+        Console.WriteLine("Esc. Back to Main Menu");
+
+        bool exit = false;
+        while (!exit)
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.Enter:
+                    // Здесь можно обработать выбранный пункт подменю
+                    break;
+                case ConsoleKey.Escape:
+                    exit = true;
+                    break;
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<MenuItem> subMenuItems = new List<MenuItem>()
+        {
+            new MenuItem("Option 1", 10.0m),
+            new MenuItem("Option 2", 20.0m),
+            new MenuItem("Option 3", 30.0m)
+        };
+
+        Menu subMenu = new Menu(subMenuItems);
+        subMenu.ExecuteSubMenu();
+
+        Order order = new Order();
+        order.StartOrder();
+    }
+}
